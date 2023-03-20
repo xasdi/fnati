a16c1fb303ef127add341b0e34f1ac7c0b31f9eb
Jakub Dąbrowski <jdabrowski.xd@gmail.com>
2023-03-20 23:04:17 +0100

    Added basic camera movement to left, now working on right movement.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var arrow;
 var start = false;
 var scrolleft = 0;
+var imageref;
+var imagestyle;
+var currentimageposition;
+var leftimagechangeposition;
+var newimageposition = 0;
 
 function startgame(){
-
+    leftimagechangeposition = document.getElementById("imagebackground");
     arrow = document.getElementById("leftscrollbutton");
+    imageref = document.getElementById("imagebackground");
+    
+    
+
     
     
     
@@ -23,20 +32,38 @@ function startloops(){
 }
 
 function checkmouseposition(){
-
+    console.log("sprawdzono")
+    imagestyle = getComputedStyle(imageref);
+    currentimageposition = imagestyle.left;
+    currentimageposition = parseInt(currentimageposition, 10);
     arrow.addEventListener("mouseover", () => {
         scrolleft = 1; 
+        console.log("myszka na przcisku");
     });
 
     arrow.addEventListener("mouseout", () => {
-        scrolleft = 0; 
+        scrolleft = 0;
+        console.log("myszka po za przciskiem");
     });
 
-    if(scrolleft==1){
-        
+    if(scrolleft==1 && currentimageposition <= -10){
+        console.log(currentimageposition);
+        newimageposition = currentimageposition + 10;
+        leftimagechangeposition.style.left = newimageposition + "px";
+    }
+
+    if(scrolleft==1 && currentimageposition <= -10){
+        console.log(currentimageposition);
+        newimageposition = currentimageposition + 10;
+        leftimagechangeposition.style.left = newimageposition + "px";
     }
 }
 
+function showzmienne(){
+    console.log(scrolleft);
+    console.log(imagechangeposition);
+
+}
 
 
 
