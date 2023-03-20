aadb60fe7a7c047cb872478832dd5c302b4d5f70
Jakub Dąbrowski <jdabrowski.xd@gmail.com>
2023-03-20 22:14:06 +0100

    First version of looking around engine

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-function lookleft(){
+var arrow;
+var start = false;
+var scrolleft = 0;
+
+function startgame(){
+
+    arrow = document.getElementById("leftscrollbutton");
     
+    
+    
+        
+    
+    
+    
+    
+     
+        
+        setInterval(checkmouseposition, 500);
 }
+
+function startloops(){
+    start = true;
+}
+
+function checkmouseposition(){
+
+    arrow.addEventListener("mouseover", () => {
+        scrolleft = 1; 
+    });
+
+    arrow.addEventListener("mouseout", () => {
+        scrolleft = 0; 
+    });
+
+    if(scrolleft==1){
+        
+    }
+}
+
+
+
+
+
+
+
+
